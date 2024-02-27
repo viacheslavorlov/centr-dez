5c22b9bb47b458f515e4791f8051d247d8d8a2ae
Orlov Viacheslav <viacheslavorlov@gmail.com>
2024-02-27 23:25:22 +0300

    Enhance layout and add typewriter effect
    This commit introduces several enhancements to the layout and components of the project. Key changes include making the phone property optional and adding description, icon, and keywords properties to the layout. The language of the layout has been switched to Russian. Additionally, a new Typewriter component has been added to create a dynamic typewriting effect for text display. The Button component has been updated for better visual appeal, and various UI components have been refined for consistency and flexibility. The styles.css file now includes a global transition effect for all elements.
    
    - Make phone property optional and add new properties to Layout
    - Switch layout language to Russian
    - Introduce Typewriter component for dynamic text effects
    - Update Button component styling for enhanced appearance
    - Refine UI components for better consistency and flexibility
    - Add global transition effect in styles.css for smoother UI transitions

--- src/widgets/MobileMenu.tsx
+++ src/widgets/MobileMenu.tsx
@@ -11,24 +11,21 @@ const MobileMenu = ({ className }: Props) => {
 
 	return isVisible ? (
 		<div
-			className={`absolute bottom-0 left-0 right-0  top-0 flex h-screen w-screen flex-col items-center justify-center gap-6 bg-primary-bg md:hidden ${className}`}>
+			className={`absolute bottom-0 left-0 right-0 z-20 top-0 flex h-screen w-screen flex-col items-center justify-center gap-6 bg-primary-bg md:hidden ${className}`}>
 			{navItems.map(item => (
 				<>
 					<div
 						key={item.url}
 						className='border text-primary-text  transition-all border-accent text-2xl px-4 py-2 rounded-2xl hover:scale-105 hover:text-white hover:bg-accent'
 						onClick={() => setIsVisible(false)}>
-						<a
-							href={item.url}>
-							{item.title}
-						</a>
+						<a href={item.url}>{item.title}</a>
 					</div>
 				</>
 			))}
 			<div
 				onClick={() => setIsVisible(false)}
 				class={
-					'rounded-full  flex justify-center items-center  m-auto bg-accent absolute right-6 top-4 h-10 w-10 '
+					'rounded-full flex justify-center items-center m-auto bg-accent absolute right-6 top-4 h-10 w-10 '
 				}>
 				<X />
 			</div>
@@ -36,7 +33,7 @@ const MobileMenu = ({ className }: Props) => {
 	) : (
 		<div
 			onClick={() => setIsVisible(prev => !prev)}
-			className='md:hidden absolute flex justify-center items-center right-6 top-4 h-10 w-10  bg-accent rounded-full'>
+			className='md:hidden absolute flex justify-center z-20 items-center right-6 top-4 h-10 w-10  bg-accent rounded-full'>
 			<Menu class={'stroke-primary-text'} />
 		</div>
 	);
